6edd90aebc3b8156c5a4cb7e00f27fe3ec9d9c97
Adithya Hulangamuwa <adithyadrck@gmail.com>
2025-03-18 00:37:43 +0530

    Setting targets UI 1st commit

--- InvoiceApp-master/InvoiceApp-master/frontend/src/pages/Dashboard.js
+++ InvoiceApp-master/InvoiceApp-master/frontend/src/pages/Dashboard.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { InvoicePage } from "./InvoicePage";
 import { QuotePage } from "./QuotePage";
+import { SetTargets } from "./target/SetTargets";
 import DashboardOutlinedIcon from "@mui/icons-material/DashboardOutlined";
 import DocumentScannerIcon from "@mui/icons-material/DocumentScanner";
 import PriceCheckIcon from "@mui/icons-material/PriceCheck";
@@ -28,12 +29,18 @@ export const Dashboard = () => {
               <PriceCheckIcon sx={{ fill: "#007EA4" }} />
               <Link to="quote">Quotation</Link>
             </li>
+            <span className="fw-semibold">Targets</span>
+            <li className="d-flex gap-2 my-3 bg-light bg-opacity-10 p-2 rounded">
+              <PriceCheckIcon sx={{ fill: "#007EA4" }} />
+              <Link to="set-targets">Set Targets</Link>
+            </li>
           </ul>
         </div>
         <div className="m-3 pl-5 content-section">
           <Routes>
             <Route path="/invoice" element={<InvoicePage />} />
             <Route path="/quote" element={<QuotePage />} />
+            <Route path="/set-targets" element={<SetTargets />} />
           </Routes>
         </div>
       </div>
